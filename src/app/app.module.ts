36ddf451807aade3ae719ce483da6d56ec5cf9b8
Robin V <dev@verhu.be>
2019-10-08 17:04:25 +0200

    split components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StatsComponent } from './stats/stats.component';
+import { LineupComponent } from './lineup/lineup.component';
+import { ScoresheetComponent } from './scoresheet/scoresheet.component';
+import { MatchComponent } from './match/match.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    StatsComponent
+    StatsComponent,
+    LineupComponent,
+    ScoresheetComponent,
+    MatchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/scoresheet/scoresheet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scoresheet',
+  templateUrl: './scoresheet.component.html',
+  styleUrls: ['./scoresheet.component.scss']
+})
+export class ScoresheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
